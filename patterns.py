44dca4278cb0211b1a0ecac68a7e317437625f70
Chris <cspollard@gmail.com>
2013-05-21 20:47:21 -0400

    added a bunch of patterns.

--- patterns.py
+++ patterns.py
@@ -1,8 +1,33 @@
 # Module that contains patters for the HexLight
-from time import sleep
+from pattern import Pattern, Animation
+from itertools import ifilter
 
-def circle(wait):
-	pass
+def all_bits(state):
+    return Pattern(repeat(state, 24))
+
+def all_on():
+    return all_bits(1)
+
+def all_off():
+    return all_bits(0)
+
+def inner_circle():
+    return Pattern(ifilter(lambda x: x > 18), range(25))
+
+def inner_circle_alt_odd():
+    return Pattern(ifilter(lambda x: x > 18 && x % 2), range(25))
+
+def inner_circle_alt_even():
+    return Pattern(ifilter(lambda x: x > 18 && not x % 2), range(25))
+
+def outer_circle():
+    return Pattern(ifilter(lambda x: x < 19), range(25))
+
+def outer_circle_alt_odd():
+    return Pattern(ifilter(lambda x: x < 19 && x % 2), range(25))
+
+def outer_circle_alt_even():
+    return Pattern(ifilter(lambda x: x < 19 && not x % 2), range(25))
 
 def wedge(wait):
 	pass
@@ -21,3 +46,12 @@ def spinin(wait,n):
 def spinall(wait,n):
 	pass
 
+def spinning_inner_circle(wait):
+    return Animation(
+            [inner_circle_alt_odd(), inner_circle_alt_even()],
+            [wait, wait])
+
+def spinning_outer_circle(wait):
+    return Animation(
+            [outer_circle_alt_odd(), outer_circle_alt_even()],
+            [wait, wait])
